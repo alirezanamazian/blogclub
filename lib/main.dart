1f418b4fef3eca68da358e5c11ac50884d831302
alirezanamazian <alirezana72@gmail.com>
2023-01-22 19:33:39 +0300

    stories

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:blogclub/data.dart';
+import 'package:blogclub/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,24 +7,29 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static const defualtFontFamily = 'Avenir';
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const primaryTextColor = Color(0xff0D253C);
+    const secondaryTextColor = Color(0xff2D4379);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        textTheme: const TextTheme(
+          headline6: TextStyle(
+              fontFamily: defualtFontFamily,
+              fontWeight: FontWeight.bold,
+              color: primaryTextColor),
+          subtitle1: TextStyle(
+              fontSize: 16,
+              fontFamily: defualtFontFamily,
+              color: secondaryTextColor),
+        ),
       ),
       home: const HomeScreen(),
     );
@@ -34,12 +41,108 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final stories = AppDatabase.stories;
+    final textTheme = Theme.of(context).textTheme;
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          physics: BouncingScrollPhysics(),
+          physics: const BouncingScrollPhysics(),
           child: Column(
-            children: [Text('data')],
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 16, 32, 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Hi, Jonathan!',
+                      style: textTheme.subtitle1,
+                    ),
+                    Image.asset(
+                      Assets.img.icons.notification.path,
+                      width: 24,
+                      height: 24,
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 0, 0, 20),
+                child: Text(
+                  'Explore today\'s',
+                  style: textTheme.headline6,
+                ),
+              ),
+              SizedBox(
+                height: 100,
+                width: size.width,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: stories.length,
+                    itemBuilder: ((context, index) {
+                      final story = stories[index];
+                      return Padding(
+                        padding: EdgeInsets.fromLTRB(
+                            index == 0 ? 32 : size.width / 30, 0, 0, 0),
+                        child: Column(
+                          children: [
+                            Stack(children: [
+                              Container(
+                                width: 68,
+                                height: 68,
+                                decoration: const BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(24)),
+                                  gradient: LinearGradient(
+                                      begin: Alignment.topCenter,
+                                      end: Alignment.bottomCenter,
+                                      colors: [
+                                        Color(0xff376AED),
+                                        Color(0xff49B0E2),
+                                        Color(0xff9CECFB),
+                                      ]),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(3),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
+                                    child: Container(
+                                      margin: EdgeInsets.all(4),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(15),
+                                        child: Image.asset(
+                                          'assets/img/stories/${story.imageFileName}',
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 0,
+                                right: 0,
+                                child: Image.asset(
+                                  'assets/img/icons/${story.iconFileName}',
+                                  height: 25,
+                                  width: 25,
+                                ),
+                              )
+                            ]),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Text(story.name)
+                          ],
+                        ),
+                      );
+                    })),
+              )
+            ],
           ),
         ),
       ),
